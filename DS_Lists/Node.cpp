123d7bae3c98c433803198989b9b325597339b21
Jonathan Valencia <darkmanp2@yahoo.com>
2016-01-24 11:09:19 -0500

    changed name of the solution and made the class NodePrac header

---
+++ DS_Lists/Node.cpp
@@ -0,0 +1,21 @@
+#include "Node.h"
+#include "stdafx.h"
+
+//////fields
+////int data;
+////NodePrac * nextNode;
+////static int listCount;
+////
+//////constructors and destructor
+////NodePrac(const NodePrac &origList);
+////NodePrac(NodePrac *node = nullptr, int data = -1);
+////~NodePrac();
+////
+//////Methods
+////void insertAfter(const NodePrac &refNode, NodePrac *insertionNode);
+////void insertBefore(const NodePrac &refNode, NodePrac *insertionNode);
+////void pushInsert(NodePrac *insertionNode);
+////void deleteNode(NodePrac &refNode);
+////NodePrac& operator==(const NodePrac &origList);
+
+NodePrac::NodePrac(const NodePrac &origList): 
